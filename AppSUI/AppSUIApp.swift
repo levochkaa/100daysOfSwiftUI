2adcf2c9d474465db7c8ad7679ba521164776330
Lev Poznyakov <levfirstgamer@mail.ru>
2022-04-20 23:25:34 +0300

    Initial Commit

---
+++ AppSUI/AppSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppSUIApp.swift
+//  AppSUI
+//
+//  Created by Лев on 20.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct AppSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppSUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AppSUI
+//
+//  Created by Лев on 20.04.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
