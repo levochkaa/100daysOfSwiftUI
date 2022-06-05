ac4d0ff4e3234536ab4ea3e3ee66a5deea56a1c8
Lev Poznyakov <levfirstgamer@mail.ru>
2022-06-05 12:14:47 +0300

    BucketList project 14

---
+++ AppSUI/Projects/BucketList.swift
@@ -0,0 +1,288 @@
+import SwiftUI
+import MapKit
+import LocalAuthentication
+
+enum LoadingState {
+    case loading, loaded, failed
+}
+
+struct Location: Identifiable, Codable, Equatable {
+    var id: UUID
+    var name: String
+    var description: String
+    let latitude: Double
+    let longitude: Double
+    var coordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
+
+    static let example = Location(id: UUID(), name: "Buckingham Palace", description: "Where Queen Elizabeth lives with her dorgis.", latitude: 51.501, longitude: -0.141)
+
+    static func ==(lhs: Location, rhs: Location) -> Bool {
+        lhs.id == rhs.id
+    }
+}
+
+struct Result: Codable {
+    let query: Query
+}
+
+struct Query: Codable {
+    let pages: [Int: Page]
+}
+
+struct Page: Codable, Comparable {
+    let pageid: Int
+    let title: String
+    let terms: [String: [String]]?
+
+    var description: String {
+        terms?["description"]?.first ?? "No further information"
+    }
+
+    static func <(lhs: Page, rhs: Page) -> Bool {
+        lhs.title < rhs.title
+    }
+}
+
+extension BucketList {
+    @MainActor class ViewModel: ObservableObject {
+
+        let savePath = FileManager.documentsDirectory.appendingPathComponent("SavedPlaces")
+
+        @Published var showAuthError = false
+        @Published var authErrorMessage = ""
+        @Published var isUnlocked = false
+        @Published var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 50, longitude: 0), span: MKCoordinateSpan(latitudeDelta: 25, longitudeDelta: 25))
+        @Published private(set) var locations: [Location]
+        @Published var selectedPlace: Location?
+
+        init() {
+            do {
+                let data = try Data(contentsOf: savePath)
+                locations = try JSONDecoder().decode([Location].self, from: data)
+            } catch {
+                locations = []
+            }
+        }
+
+        func addLocation() {
+            let newLocation = Location(id: UUID(), name: "New location", description: "", latitude: mapRegion.center.latitude, longitude: mapRegion.center.longitude)
+            locations.append(newLocation)
+            save()
+        }
+
+        func update(location: Location) {
+            guard let selectedPlace = selectedPlace else { return }
+            if let index = locations.firstIndex(of: selectedPlace) {
+                locations[index] = location
+            }
+            save()
+        }
+
+        func save() {
+            do {
+                let data = try JSONEncoder().encode(locations)
+                try data.write(to: savePath, options: [.atomic, .completeFileProtection])
+            } catch {
+                print("Unable to save data.")
+            }
+        }
+
+        func authenticate() {
+            let context = LAContext()
+            var error: NSError?
+
+            if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+                let reason = "Please authenticate yourself to unlock your places."
+
+                context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
+
+                    Task { @MainActor in
+                        if success {
+                            self.isUnlocked = true
+                        } else {
+                            self.authErrorMessage = authenticationError?.localizedDescription ?? "Unknown error"
+                            self.showAuthError = true
+                        }
+                    }
+                }
+            } else {
+                // no biometrics
+            }
+        }
+    }
+}
+
+struct BucketList: View {
+
+    @StateObject private var viewModel = ViewModel()
+
+    var body: some View {
+        ZStack {
+            if viewModel.isUnlocked {
+                Map(coordinateRegion: $viewModel.mapRegion, annotationItems: viewModel.locations) { location in
+                    MapAnnotation(coordinate: location.coordinate) {
+                        VStack {
+                            Image(systemName: "star.circle")
+                                .resizable()
+                                .foregroundColor(.red)
+                                .frame(width: 44, height: 44)
+                                .background(.white)
+                                .clipShape(Circle())
+
+                            Text(location.name)
+                                .fixedSize()
+                        }
+                        .onTapGesture {
+                            viewModel.selectedPlace = location
+                        }
+                    }
+                }
+                .ignoresSafeArea()
+                Circle()
+                    .fill(.blue)
+                    .opacity(0.3)
+                    .frame(width: 32, height: 32)
+                VStack {
+                    Spacer()
+                    HStack {
+                        Spacer()
+                        Button {
+                            viewModel.addLocation()
+                        } label: {
+                            Image(systemName: "plus")
+                                .padding()
+                                .background(.black.opacity(0.75))
+                                .foregroundColor(.white)
+                                .font(.title)
+                                .clipShape(Circle())
+                                .padding(.trailing)
+                        }
+                    }
+                }
+            } else {
+                Button("Unlock Places") {
+                    viewModel.authenticate()
+                }
+                .padding()
+                .background(.blue)
+                .foregroundColor(.white)
+                .clipShape(Capsule())
+            }
+        }
+        .sheet(item: $viewModel.selectedPlace) { place in
+            EditBucketListView(location: place) { newLocation in
+                viewModel.update(location: newLocation)
+            }
+        }
+        .alert(Text("Try again"), isPresented: $viewModel.showAuthError) {
+            //
+        } message: {
+            Text(viewModel.authErrorMessage)
+        }
+    }
+}
+
+extension EditBucketListView {
+    @MainActor class ViewModel: ObservableObject {
+        @Published var location: Location
+        @Published var name: String
+        @Published var description: String
+        @Published var loadingState: LoadingState = .loading
+        @Published var pages = [Page]()
+
+        init(location: Location, name: String, description: String) {
+            self.location = location
+            self.name = name
+            self.description = description
+        }
+    }
+}
+
+struct EditBucketListView: View {
+
+    var onSave: (Location) -> Void
+
+    @StateObject var viewModel: ViewModel
+
+    @Environment(\.dismiss) var dismiss
+
+    init(location: Location, onSave: @escaping (Location) -> Void) {
+        self.onSave = onSave
+        _viewModel = StateObject(wrappedValue: ViewModel(location: location, name: location.name, description: location.description))
+    }
+
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Place name", text: $viewModel.name)
+                    TextField("Description", text: $viewModel.description)
+                }
+                Section("Nearby…") {
+                    switch viewModel.loadingState {
+                        case .loaded:
+                            ForEach(viewModel.pages, id: \.pageid) { page in
+                                Text(page.title)
+                                    .font(.headline)
+                                + Text(": ") +
+                                Text(page.description)
+                                    .italic()
+                            }
+                        case .loading:
+                            Text("Loading…")
+                        case .failed:
+                            Text("Please try again later.")
+                    }
+                }
+            }
+            .navigationTitle("Place details")
+            .toolbar {
+                Button("Save") {
+                    var newLocation = viewModel.location
+                    newLocation.id = UUID()
+                    newLocation.name = viewModel.name
+                    newLocation.description = viewModel.description
+                    onSave(newLocation)
+                    dismiss()
+                }
+            }
+            .task {
+                await fetchNearbyPlaces()
+            }
+        }
+    }
+
+    func fetchNearbyPlaces() async {
+        let urlString = "https://en.wikipedia.org/w/api.php?ggscoord=\(viewModel.location.coordinate.latitude)%7C\(viewModel.location.coordinate.longitude)&action=query&prop=coordinates%7Cpageimages%7Cpageterms&colimit=50&piprop=thumbnail&pithumbsize=500&pilimit=50&wbptterms=description&generator=geosearch&ggsradius=10000&ggslimit=50&format=json"
+
+        guard let url = URL(string: urlString) else {
+            print("Bad URL: \(urlString)")
+            return
+        }
+
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            let items = try JSONDecoder().decode(Result.self, from: data)
+            viewModel.pages = items.query.pages.values.sorted()
+            viewModel.loadingState = .loaded
+        } catch {
+            viewModel.loadingState = .failed
+        }
+    }
+}
+
+extension FileManager {
+    static var documentsDirectory: URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
+}
+
+struct BucketList_Previews: PreviewProvider {
+    static var previews: some View {
+        BucketList()
+            .preferredColorScheme(.dark)
+    }
+}
