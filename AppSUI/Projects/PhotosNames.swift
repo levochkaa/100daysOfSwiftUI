27f64be3bb93ae15d418a74d874554f483df27a8
Lev Poznyakov <levfirstgamer@mail.ru>
2022-06-08 18:43:22 +0300

    day 78 update

--- AppSUI/Projects/PhotosNames.swift
+++ AppSUI/Projects/PhotosNames.swift
@@ -1,9 +1,37 @@
 import SwiftUI
+import CoreLocation
+import MapKit
+
+class LocationFetcher: NSObject, CLLocationManagerDelegate {
+    let manager = CLLocationManager()
+    var lastKnownLocation: CLLocationCoordinate2D?
+
+    override init() {
+        super.init()
+        manager.delegate = self
+        self.start()
+    }
+
+    func start() {
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+    }
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        lastKnownLocation = locations.first?.coordinate
+    }
+}
 
 struct ImageWithName: Identifiable, Codable, Equatable {
     var id: UUID
     var name: String
     var imageData: Data
+    let latitude: Double
+    let longitude: Double
+
+    var coordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
 
     var image: Image? {
         if let uiImage = UIImage(data: imageData) {
@@ -33,9 +61,9 @@ extension PhotosNames {
             }
         }
 
-        func addImage(image: UIImage, name: String) {
+        func addImage(image: UIImage, name: String, location: CLLocationCoordinate2D) {
             if let pngData = image.pngData() {
-                let newImage = ImageWithName(id: UUID(), name: name, imageData: pngData)
+                let newImage = ImageWithName(id: UUID(), name: name, imageData: pngData, latitude: location.latitude, longitude: location.longitude)
                 images.append(newImage)
                 save()
             }
@@ -54,6 +82,8 @@ extension PhotosNames {
 
 struct PhotosNames: View {
 
+    let locationFetcher = LocationFetcher()
+
     @State private var inputImage: UIImage?
     @State private var showingImagePicker = false
 
@@ -63,7 +93,7 @@ struct PhotosNames: View {
         NavigationView {
             List(viewModel.images) { image in
                 if let wrappedImage = image.image {
-                    NavigationLink(destination: PhotosNamesDetailView(image: wrappedImage, name: image.name)) {
+                    NavigationLink(destination: PhotosNamesDetailView(imageWithName: image)) {
                         HStack {
                             wrappedImage
                                 .resizable()
@@ -90,25 +120,43 @@ struct PhotosNames: View {
         .onChange(of: inputImage) { _ in
             if let image = inputImage {
                 alertTextField(title: "Image name", message: "Please enter name for the imported image", hintText: "Unknown", primaryTitle: "Enter", secondaryTitle: "Cancel", primaryAction: { text in
-                    viewModel.addImage(image: image, name: text == "" ? "Unknown" : text)
+                    if let location = self.locationFetcher.lastKnownLocation {
+                        viewModel.addImage(image: image, name: text == "" ? "Unknown" : text, location: location)
+                    }
                 }, secondaryAction: {
-                    viewModel.addImage(image: image, name: "Unknown")
+                    //
                 })
             }
         }
     }
 }
 
+struct Annotation: Identifiable {
+    var id = UUID()
+}
+
 struct PhotosNamesDetailView: View {
 
-    let image: Image
-    let name: String
+    let locations = [Annotation()]
+
+    @State var imageWithName: ImageWithName
+    @State private var mapRegion: MKCoordinateRegion
+
+    init(imageWithName: ImageWithName) {
+        _imageWithName = State(wrappedValue: imageWithName)
+        _mapRegion = State(wrappedValue: MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: imageWithName.latitude, longitude: imageWithName.longitude), span: MKCoordinateSpan(latitudeDelta: 5, longitudeDelta: 5)))
+    }
 
     var body: some View {
-        image
-            .resizable()
-            .scaledToFit()
-            .navigationTitle(name)
+        VStack {
+            imageWithName.image?
+                .resizable()
+                .scaledToFit()
+            Map(coordinateRegion: $mapRegion, annotationItems: locations) { _ in
+                MapMarker(coordinate: imageWithName.coordinate)
+            }
+        }
+        .navigationTitle(imageWithName.name)
     }
 }
 
