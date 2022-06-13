0262a3f9959e255dce2010d344216ffe1bbcdcb4
Lev Poznyakov <levfirstgamer@mail.ru>
2022-06-13 16:23:20 +0300

    Clear ContentView.swift

--- AppSUI/ContentView.swift
+++ AppSUI/ContentView.swift
@@ -1,29 +1,11 @@
 import SwiftUI
 
-struct User: Identifiable {
-    var id = "Taylor Swift"
-}
-
 struct ContentView: View {
 
-    @State private var selectedUser: User? = nil
-    @State private var isShowingUser = false
+    //
 
     var body: some View {
-        NavigationView {
-            Text("Hello, World!")
-                .onTapGesture {
-                    isShowingUser = true
-                }
-                .sheet(item: $selectedUser) { user in
-                    Text(user.id)
-                }
-                .alert("Welcome", isPresented: $isShowingUser) { }
-
-            Text("hi")
-
-            Text("By")
-        }
+        Text("Hi, Mom")
     }
 }
 
