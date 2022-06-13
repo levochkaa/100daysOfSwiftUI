00ce0a4d00c90da9a16a68ace2dfc2a2d33e6f1d
Lev Poznyakov <levfirstgamer@mail.ru>
2022-06-13 16:21:30 +0300

    Files for 19 project

--- AppSUI/ContentView.swift
+++ AppSUI/ContentView.swift
@@ -1,11 +1,29 @@
 import SwiftUI
 
+struct User: Identifiable {
+    var id = "Taylor Swift"
+}
+
 struct ContentView: View {
 
-    //
+    @State private var selectedUser: User? = nil
+    @State private var isShowingUser = false
 
     var body: some View {
-        Text("Hi, Mom")
+        NavigationView {
+            Text("Hello, World!")
+                .onTapGesture {
+                    isShowingUser = true
+                }
+                .sheet(item: $selectedUser) { user in
+                    Text(user.id)
+                }
+                .alert("Welcome", isPresented: $isShowingUser) { }
+
+            Text("hi")
+
+            Text("By")
+        }
     }
 }
 
