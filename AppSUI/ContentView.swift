d0b4c76b0c25ac939f04b2dd5ff42523835e8034
Lev Poznyakov <levochkaaa@icloud.com>
2022-12-17 13:07:23 +0300

    idk

--- AppSUI/ContentView.swift
+++ AppSUI/ContentView.swift
@@ -2,10 +2,50 @@ import SwiftUI
 
 struct ContentView: View {
 
-    //
+    var frame: CGFloat {
+        isZoomed ? 300 : 40
+    }
+
+    @Namespace private var animation
+
+    @State private var isZoomed = false
 
     var body: some View {
-        Text("Hi, Mom")
+        VStack {
+            if !isZoomed {
+                Spacer()
+            }
+
+            VStack {
+                HStack {
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(Color.blue)
+                        .frame(width: frame, height: frame)
+                        .padding(.top, isZoomed ? 20 : 0)
+                    if !isZoomed {
+                        Text("Taylor Swift - 1989")
+                            .font(.headline)
+                            .matchedGeometryEffect(id: "AlbumTitle", in: animation)
+                        Spacer()
+                    }
+                }
+                if isZoomed {
+                    Text("Taylor Swift - 1989")
+                        .font(.headline)
+                        .matchedGeometryEffect(id: "AlbumTitle", in: animation)
+                        .padding(.bottom, 60)
+                    Spacer()
+                }
+            }
+            .onTapGesture {
+                withAnimation(.spring()) {
+                    isZoomed.toggle()
+                }
+            }
+            .padding()
+            .frame(maxWidth: .infinity)
+            .frame(height: isZoomed ? 400 : 60)
+        }
     }
 }
 
